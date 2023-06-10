62c732943d3b8cf08f16a23ce252f0c5dbbf2072
themasterid <77408415+themasterid@users.noreply.github.com>
2023-06-10 15:12:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.network.urlrequest import UrlRequest
 
+class LoginScreen(BoxLayout):
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        
+    def login(self):
+        username = self.ids.username.text
+        password = self.ids.password.text
+        auth_data = {'username': username, 'password': password}
+        headers = {'Content-type': 'application/json'}
+        UrlRequest('http://127.0.0.l/auth/login/', on_success=self.login_success,
+                   on_failure=self.login_failure, req_body=json.dumps(auth_data), req_headers=headers)
+        
+    def login_success(self, req, result):
+        # Обработка успешного входа пользователя
+        print("Успешный вход:", result)
+        
+    def login_failure(self, req, result):
+        # Обработка неудачного входа пользователя
+        print("Неудачный вход:", result)
 
-class MainApp(App):
+class MyApp(App):
     def build(self):
-        button = Button(
-            text='Старт приложения для Автомедика!',
-            size_hint=(.5, .5),
-            pos_hint={'center_x': .5, 'center_y': .5})
-        button.bind(on_press=self.on_press_button)
-
-        return button
-
-    def on_press_button(self, instance):
-        print('Вы нажали на кнопку!')
-
+        return LoginScreen()
 
 if __name__ == '__main__':
-    app = MainApp()
-    app.run()
+    MyApp().run()
